7cd0cc59b7bed80b107c8010dd9ed93bd9db23d4
Gayan Kaushalya <gaykaush@gmail.com>
2023-12-07 20:18:08 +0530

    Added a new file

---
+++ IEEEXtreme 17.0/caesarredux.py
@@ -0,0 +1,36 @@
+letters = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5,
+           'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11,
+           'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17,
+           's':18, 't':19, 'u':20, 'v':21, 'w':22, 'x':23,
+           'y':24, 'z':25}
+
+keys = list(letters.keys())
+values = list(letters.values())
+
+results = []
+
+for i in range (int(input())):
+    shift = int(input())
+    message = input()
+    
+    result = ""
+    
+    if "the" in message.split():
+        for j in range (len(message)):
+            if message[j] in letters:
+                result += (keys[values.index((letters[message[j]] - shift) % 26)])
+            else:
+                result += message[j]
+        results.append(result)
+                
+    else:
+        for j in range (len(message)):
+            if message[j] in letters:
+                result += (keys[values.index((letters[message[j]] + shift) % 26)])
+            else:
+                result += message[j]
+
+        results.append(result)
+        
+for i in range (len(results)):
+    print(results[i])
