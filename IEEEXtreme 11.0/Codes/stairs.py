871bed546420401c524274d9e50374849e672db9
Gayan Kaushalya <gaykaush@gmail.com>
2023-12-31 01:12:24 +0530

    Added Running Up Stairs

---
+++ IEEEXtreme 11.0/Codes/stairs.py
@@ -0,0 +1,23 @@
+'''
+This part had to be added to the code to pass the trst cases. 
+
+Otherwise it gives the following error:
+    ValueError: Exceeds the limit (4300) for integer string conversion: value has 5432 digits.
+'''
+
+import sys
+sys.set_int_max_str_digits(0)
+
+fib = {2: 2, 1: 1}
+numbers = []
+
+y = int(input())
+
+for j in range(y):
+    x = int(input())
+    numbers.append(x)
+    for i in range(3, x + 1):
+        if i not in fib:
+            fib[i] = fib[i - 1] + fib[i - 2]
+for i in numbers:
+    print(fib[i])
--- IEEEXtreme 11.0/README.md
+++ IEEEXtreme 11.0/README.md
@@ -1,4 +1,5 @@
 - [Blackgate Penitentiary](#Blackgate-Penitentiary)
+- [Running Up Stairs](#Running-Up-Stairs)
 
 
 # Blackgate Penitentiary
@@ -7,3 +8,11 @@
 ![Alt text](Images/Blackgate%20Penitentiary%202.png)
 
 [Answer](Codes/blackgate.py)
+
+
+# Running Up Stairs
+
+![Alt text](Images/Running%20Up%20Stairs%201.png)
+![Alt text](Images/Running%20Up%20Stairs%202.png)
+
+[Answer](Codes/stairs.py)
