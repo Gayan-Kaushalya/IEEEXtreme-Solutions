e3c0561ca877151806ab9e8e6366b00ef617f7e5
Gayan Kaushalya <gaykaush@gmail.com>
2024-06-18 12:17:20 +0530

    Added Rigged Dice

---
+++ IEEEXtreme 15.0/Codes/riggeddice.py
@@ -0,0 +1,38 @@
+for g in range(int(input())):
+    alice_has_dice_a = True
+
+    alice_score = 0
+    bob_score = 0
+    
+    first_die_six_count = 0
+    second_die_six_count = 0
+
+    for r in range(int(input())):
+        a, b = map(int, input().split())
+
+        alice_score += a
+        bob_score += b
+        
+        if alice_has_dice_a:
+            if a == 6:
+                first_die_six_count += 1
+                
+            if b == 6:
+                second_die_six_count += 1
+                
+        else:
+            if a == 6:
+                second_die_six_count += 1
+                
+            if b == 6:
+                first_die_six_count += 1
+                
+                
+        if alice_score != bob_score:
+            alice_has_dice_a = not alice_has_dice_a
+
+    if first_die_six_count > second_die_six_count:
+        print(1)
+        
+    else:
+        print(2)
--- IEEEXtreme 15.0/README.md
+++ IEEEXtreme 15.0/README.md
@@ -1,4 +1,5 @@
 - [Xtreme Winners](#Xtreme-Winners)
+- [Rigged Dice](#Rigged-Dice)
 
 <hr>
 
@@ -9,5 +10,14 @@
 
 [Answer (Python - 72.73%)](Codes/xtremewinners.py) <br>
 [Answer (Ruby - 93.02%)](Codes/xtremewinners.rb)
+
+<br/><hr>
+
+# Rigged Dice
+
+![Alt text](Images/Rigged%20Dice%201.png)
+![Alt text](Images/Rigged%20Dice%202.png)
+
+[Answer](Codes/riggeddice.py)
 
 <br/><hr>
